100f635cbbf5c1a72ad74fc571ebc341cd6f36a3
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-07-02 12:00:05 +0500

    [+] Added Todo list app

--- .assigmnment_practice/ass_5/lib/data.dart
+++ .assigmnment_practice/ass_5/lib/data.dart
@@ -2,25 +2,25 @@ List<Map<String, dynamic>> data = [
   {
     "title": "task title",
     "isDone": false,
-    "taskDesc": "lorem ipsum description",
+    "desc": "lorem ipsum description",
     "dueDate": 2
   },
   {
     "title": "task title",
     "isDone": false,
-    "taskDesc": "lorem ipsum description",
+    "desc": "lorem ipsum description",
     "dueDate": 1
   },
   {
     "title": "task title",
     "isDone": false,
-    "taskDesc": "lorem ipsum description",
+    "desc": "lorem ipsum description",
     "dueDate": 5
   },
   {
     "title": "task title",
     "isDone": false,
-    "taskDesc": "lorem ipsum description",
+    "desc": "lorem ipsum description",
     "dueDate": 10
   }
 
--- .assigmnment_practice/ass_5/lib/main.dart
+++ .assigmnment_practice/ass_5/lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'data.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 void main() => runApp(MaterialApp(
+  title: "Todo List App - ARK",
       debugShowCheckedModeBanner: false,
       home: const MyApp(),
       theme: ThemeData(primarySwatch: Colors.purple),
@@ -15,83 +20,217 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Map<String, dynamic>> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadTasks();
+  }
+
+  void addTaskDialog() {
+  String title = '';
+  String desc = '';
+  int? daysRemaining;
+  bool showError = false;
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Add Task"),
+        content: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: "Task title"),
+              onChanged: (value) {
+                setState(() {
+                  title = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: "Description"),
+              onChanged: (value) {
+                desc = value;
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: "Due in...(days)"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              onChanged: (value) {
+                setState(() {
+                  daysRemaining = int.tryParse(value);
+                });
+              },
+            ),
+            if (showError)
+              const Text(
+                'Please enter a title and number of days remaining',
+                style: TextStyle(color: Colors.red),
+              ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                if (title.isNotEmpty && daysRemaining != null) {
+                  final newTask = {
+                    'title': title,
+                    'desc': desc,
+                    'dueDate': daysRemaining,
+                    'isDone': false,
+                  };
+                  tasks.add(newTask);
+                  Navigator.pop(context);
+                } else {
+                  showError = true;
+                }
+              });
+            },
+            child: const Text("Add"),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          addTaskDialog();
+        },
+        label: const Text("New task"),
+        icon: const Icon(Icons.add_rounded),
+      ),
       appBar: AppBar(
         title: const Text("Todo List App"),
         centerTitle: true,
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          final _data = data[index];
-          return Task(data: _data, index: index);
+          final data = tasks[index];
+          return Task(
+              data: data,
+              index: index,
+              onTaskStatusChanged: onTaskStatusChanged);
         },
-        itemCount: data.length,
-        // Separator, requires Listview.separated()
-        // separatorBuilder: (context, index) => const Divider(),
+        itemCount: tasks.length,
       ),
     );
+  }
+
+  Future<void> loadTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final taskList = prefs.getStringList('tasks');
+    if (taskList != null) {
+      setState(() {
+        tasks = taskList
+            .map((taskJson) => jsonDecode(taskJson) as Map<String, dynamic>)
+            .toList();
+      });
+    }
+  }
+
+  Future<void> saveTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final taskList = tasks.map((task) => jsonEncode(task)).toList();
+    await prefs.setStringList('tasks', taskList);
+  }
+
+  void onTaskStatusChanged() {
+    saveTasks();
   }
 }
 
 class Task extends StatefulWidget {
-  final dynamic data;
+  final Map<String, dynamic> data;
   final int index;
-  const Task({super.key, required this.data, required this.index});
+  final VoidCallback onTaskStatusChanged;
+
+  const Task({
+    Key? key,
+    required this.data,
+    required this.index,
+    required this.onTaskStatusChanged,
+  }) : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
 }
 
 class _TaskState extends State<Task> {
-  late bool _isDone;
-  late Color _bgColor;
+  late bool isDone;
+  late Color bgColor;
 
   @override
   void initState() {
     super.initState();
-    // Initialize _isDone with the value from widget.data['isDone']
-    _isDone = widget.data['isDone'] ?? false;
-    _bgColor = Colors.white;
+    isDone = widget.data['isDone'] ?? false;
+    bgColor = Colors.white;
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: _bgColor,
+      color: bgColor,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             ListTile(
+              leading: const Icon(Icons.file_copy_sharp),
               title: Text("Task ${widget.index + 1} - ${widget.data['title']}"),
-              subtitle: Text(
-                  "Due in ${widget.data['dueDate']} ${widget.data['dueDate'] > 1 ? 'days' : 'day'}"),
+              subtitle: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${widget.data['desc']}",
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Due in (${widget.data['dueDate']}) ${widget.data['dueDate'] > 1 ? 'days' : 'day'}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
               trailing: Checkbox(
-                value: _isDone,
+                value: isDone,
                 onChanged: (newValue) {
                   setState(() {
-                    // Update both the local _isDone state and widget.data['isDone']
-                    _isDone = newValue ?? false;
+                    isDone = newValue ?? false;
                     widget.data['isDone'] = newValue;
-      
-                    // Change the color of the card
-                    widget.data['isDone']
-                        ? _bgColor = Colors.grey[200]!
-                        : _bgColor = Colors.white;
+                    widget.onTaskStatusChanged();
+
+                    bgColor = widget.data['isDone']
+                        ? Colors.grey[200]!
+                        : Colors.white;
                   });
                 },
               ),
             ),
-            const Divider(
-              color: Colors.black26,
-            ),
-            const ListTile(
-              leading: Icon(Icons.file_copy_sharp),
-              title: Text("Description of the task"),
-              subtitle: Text("Description"),
-            )
           ],
         ),
       ),
